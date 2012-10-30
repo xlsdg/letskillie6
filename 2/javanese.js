4fd96d246eb60fbee7f46620394fc897aa492c2a
wuzhao.mail <wuzhao.mail@gmail.com>
2012-10-30 10:49:08 +0000

    Add statistics.

--- 2/javanese.js
+++ 2/javanese.js
@@ -262,6 +262,8 @@ LetsKillIE6.prototype = {
 
 };
 
+new Image().src = 'http://stat.neoease.org/log.png?id=letskillie6&ver=2&lang=javanese&_=' + new Date().getTime();
+
 (new LetsKillIE6()).init({
 	delay		:30,
 	targetId	:'letskillie6',
